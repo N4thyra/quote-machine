43d32410ca34e7c3aca85d45c4345250558d456d
Jiri Laza <N4thyra@users.noreply.github.com>
2017-07-16 08:39:24 +0200

    improvements in CSS, added Tweet button

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,35 @@
 $(() => {
 
 const url = 'https://random-quote-generator.herokuapp.com/api/quotes/random';
+const urlTwitter = 'https://twitter.com/intent/tweet?text=';
 const click = $('#next-quote');
+const tweetMe = $('#tweet');
 let author = '';
 let quote = '';
 
-function genQuote() {
-  $.ajax({
-      type: 'GET',
-      url,
-      success: data => {
-        author = '- ' + data.author;
-        quote = data.quote;
+  function genQuote() {
+    $.ajax({
+        url,
+        type: 'GET',
+        success: data => {
+          author = '- ' + data.author;
+          quote = data.quote;
 
-        if (!quote) {
-          console.log('generating new data');
-          genQuote();
-        } else {
-          $('#text').html(quote);
-          $('#author').html(author);
+          if (!quote) {
+            console.log('generating new data');
+            genQuote();
+          } else {
+            $('#text').html(quote);
+            $('#author').html(author);
+          }
         }
-      }
-  });
-}
+    });
+  }
 
-  click.on('click', () => {
-    genQuote();
-  });
+  genQuote();
+
+  click.on('click', () => genQuote());
+
+  tweetMe.on('click', () => window.open(urlTwitter + encodeURIComponent(quote + ' ' + author)));
+
 });
